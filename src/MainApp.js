6a528c0f4ee81ae91c5e9dfb839f4ccba8188e26
karim <karim.boulad0@gmail.com>
2023-12-28 22:56:44 +0200

    fix error

--- src/MainApp.js
+++ src/MainApp.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Pusher from "pusher-js";
 import App from "./App";
 import "./index.css";
-import "./Css/form.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MenuContext from "./context/MenuContext";
